65213d265142e0891f3dcba52e1809c40524f5ee
David Čapčuch <d.capcuch@gmail.com>
2025-08-02 12:01:53 +0200

    feat: Init solution

---
+++ src/DotNetAtlas.Api/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+namespace DotNetAtlas.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorPages();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+        
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapStaticAssets();
+        app.MapRazorPages()
+            .WithStaticAssets();
+        
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
+
+        app.Run();
+    }
+}
