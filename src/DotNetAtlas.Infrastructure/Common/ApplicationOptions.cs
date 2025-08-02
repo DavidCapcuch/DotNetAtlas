b35b27ebc71b58db67802d027803cee3adcc222e
David Čapčuch <d.capcuch@gmail.com>
2025-08-02 17:42:53 +0200

    feat: Add observability (OTEL, Grafana, Jaeger, Prometheus, seq, optional Kibana), Index page

---
+++ src/DotNetAtlas.Infrastructure/Common/ApplicationOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetAtlas.Infrastructure.Common
+{
+    public class ApplicationOptions
+    {
+        public const string SECTION = "Application";
+
+        public required string AppName { get; set; }
+        public required bool CacheEnabled { get; set; }
+    }
+}
---
+++ src/DotNetAtlas.Infrastructure/Observability/DotNetAtlasInstrumentation.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using DotNetAtlas.Application.Observability;
+using DotNetAtlas.Infrastructure.Common;
+using Microsoft.Extensions.Options;
+
+namespace DotNetAtlas.Infrastructure.Observability
+{
+    public class DotNetAtlasInstrumentation : IDotNetAtlasInstrumentation
+    {
+        private readonly ActivitySource _activitySource;
+
+        public DotNetAtlasInstrumentation(IOptions<ApplicationOptions> options, IMeterFactory meterFactory)
+        {
+            var version = ApplicationInfo.Version;
+
+            var appName = options.Value.AppName;
+            _activitySource = new ActivitySource(appName, version);
+
+            var appMeter = meterFactory.Create($"{appName}.application", version);
+        }
+
+        public Activity StartActivity(string name, ActivityKind activityKind = ActivityKind.Internal)
+            => _activitySource.StartActivity(name, activityKind) ?? new Activity(name).Start();
+
+        public ActivitySource GetActivitySource() => _activitySource;
+    }
+}
