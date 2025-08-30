968a6a8c36164ade450af8cd66d0353313d6c724
DavidCapcuch <d.capcuch@gmail.com>
2025-08-30 21:12:31 +0200

    feat: Implement SendFeedback endpoint and related command, handler, and validation

---
+++ src/DotNetAtlas.Application/Feedback/SendFeedback/SendFeedbackCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using DotNetAtlas.Application.Common.CQS;
+
+namespace DotNetAtlas.Application.Feedback.SendFeedback
+{
+    public class SendFeedbackCommand : ICommand<Guid>
+    {
+        /// <summary>
+        /// Feedback message about the weather forecast.
+        /// </summary>
+        public required string Feedback { get; set; }
+        public required byte Rating { get; set; }
+
+        [FastEndpoints.FromClaim(ClaimTypes.NameIdentifier)]
+        public Guid UserId { get; set; }
+    }
+}
