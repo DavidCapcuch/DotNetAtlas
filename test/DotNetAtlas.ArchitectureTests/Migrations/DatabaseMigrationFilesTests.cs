eed5accc93a582795333500890d1e96eb624d567
DavidCapcuch <d.capcuch@gmail.com>
2025-08-30 21:17:03 +0200

    test: Add Clean Architecture tests, EF core and Flyway migration sync tests

---
+++ test/DotNetAtlas.ArchitectureTests/Migrations/DatabaseMigrationFilesTests.cs
@@ -0,0 +1,25 @@
+﻿using AwesomeAssertions;
+
+namespace DotNetAtlas.ArchitectureTests.Migrations
+{
+    public class DatabaseMigrationFilesTests
+    {
+        [Fact]
+        public void EfCoreMigrations_ShouldBeEqualToVersionedFlywayMigrations()
+        {
+            // Arrange
+            var migrationsCount = Directory
+                .GetFiles(DatabasePaths.EfMigrationsDirectory, "2*.cs", SearchOption.TopDirectoryOnly)
+                .Count(f => !f.EndsWith(".Designer.cs", StringComparison.OrdinalIgnoreCase));
+
+            var sqlCount = Directory
+                .GetFiles(
+                    DatabasePaths.FlywayMigrationsDirectory,
+                    "V*.sql",
+                    SearchOption.TopDirectoryOnly).Length;
+
+            // Assert
+            migrationsCount.Should().Be(sqlCount);
+        }
+    }
+}
