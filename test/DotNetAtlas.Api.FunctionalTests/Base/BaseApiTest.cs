111facb00253ef3c1a604e85fc9feb2fcd7f25a3
David Čapčuch <d.capcuch@gmail.com>
2025-08-30 21:35:40 +0200

    test: Add API test Fixture and Base test class with DB testcontainer, Flyway migrations, fake user JWT Tokens and Observability

---
+++ test/DotNetAtlas.Api.FunctionalTests/Base/BaseApiTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+using Bogus;
+using DotNetAtlas.Infrastructure.Persistence.Database;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Trace;
+using Serilog.Sinks.XUnit.Injectable.Abstract;
+
+namespace DotNetAtlas.Api.FunctionalTests.Base
+{
+    public abstract class BaseApiTest : IAsyncLifetime
+    {
+        private readonly Func<Task> _resetDatabases;
+        protected readonly WeatherForecastContext DbContext;
+        protected readonly IServiceScope Scope;
+        protected readonly HttpClient NonAuthClient;
+        protected readonly HttpClient DevClient;
+        protected readonly HttpClient PlebClient;
+        private readonly Activity? _testActivity;
+
+        protected BaseApiTest(ApiTestFixture app, ITestOutputHelper testOutputHelper)
+        {
+            var outputSink = app.Services.GetRequiredService<IInjectableTestOutputSink>();
+            outputSink.Inject(testOutputHelper);
+
+            Randomizer.Seed = new Random(420_69);
+            _resetDatabases = app.ResetDatabasesAsync;
+            Scope = app.Services.CreateScope();
+            DbContext = Scope.ServiceProvider.GetRequiredService<WeatherForecastContext>();
+
+            // In local Jaeger, you will see a trace operation with name of each test method that you can examine.
+            // Inspired by https://github.com/martinjt/unittest-with-otel/tree/main but fixed http propagation below
+            _testActivity = app.Instrumentation.StartActivity(TestContext.Current.TestMethod!.MethodName);
+            NonAuthClient = app.CreateClient(httpClient =>
+            {
+                httpClient.DefaultRequestHeaders.Add("traceparent", _testActivity?.Id);
+            });
+            DevClient = app.CreateClient(httpClient =>
+            {
+                httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", FakeTokenCreator.GetAdminUserToken());
+                httpClient.DefaultRequestHeaders.Add("traceparent", _testActivity?.Id);
+            });
+            PlebClient = app.CreateClient(httpClient =>
+            {
+                httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", FakeTokenCreator.GetNormalUserToken());
+                httpClient.DefaultRequestHeaders.Add("traceparent", _testActivity?.Id);
+            });
+        }
+
+        public ValueTask InitializeAsync()
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            var tracerProvider = Scope.ServiceProvider.GetRequiredService<TracerProvider>();
+            tracerProvider.ForceFlush();
+            _testActivity?.Stop();
+            await _resetDatabases();
+            Scope.Dispose();
+        }
+    }
+}
---
+++ test/DotNetAtlas.Api.FunctionalTests/Base/FakeTokenCreator.cs
@@ -0,0 +1,52 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using DotNetAtlas.Api.Common.Authentication;
+
+namespace DotNetAtlas.Api.FunctionalTests.Base
+{
+    /// <summary>
+    /// In appsettings.Testing, issuer validation is turned off,
+    /// else IDM would need to be permanently running for logins etc.
+    /// </summary>
+    public class FakeTokenCreator
+    {
+        public static string GetAdminUserToken()
+        {
+            var userName = "dev@dotnetatlas.com";
+            var roles = new[] {Roles.DEVELOPER};
+
+            return CreateToken(userName, roles);
+        }
+
+        public static string GetNormalUserToken()
+        {
+            var userName = "pleb@dotnetatlas.com";
+            var roles = new string[] { };
+
+            return CreateToken(userName, roles);
+        }
+
+        private static string CreateToken(string userName, string[] roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userName),
+                new Claim(ClaimTypes.NameIdentifier, Guid.CreateVersion7().ToString())
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var token = new JwtSecurityToken(
+                issuer: "NOT CHECKED IN TESTING",
+                audience: "NOT CHECKED IN TESTING",
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: null,
+                claims: claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
