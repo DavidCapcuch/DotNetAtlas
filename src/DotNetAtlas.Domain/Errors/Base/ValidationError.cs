8d4a31d0b4382cd32d29c797cf554be38072bde3
DavidCapcuch <d.capcuch@gmail.com>
2025-08-30 20:19:58 +0200

    feat: Add DDD base for entity, value objects, domain errors

---
+++ src/DotNetAtlas.Domain/Errors/Base/ValidationError.cs
@@ -0,0 +1,13 @@
+namespace DotNetAtlas.Domain.Errors.Base
+{
+    public class ValidationError : DomainError
+    {
+        public string PropertyName { get; }
+
+        public ValidationError(string propertyName, string errorMessage, string errorCode)
+            : base(errorMessage, errorCode)
+        {
+            PropertyName = propertyName;
+        }
+    }
+}
