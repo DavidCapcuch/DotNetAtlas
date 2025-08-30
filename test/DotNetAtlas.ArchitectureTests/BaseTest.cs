c13924a4e5101322c017c38ae0a46bc9d42a56ac
DavidCapcuch <d.capcuch@gmail.com>
2025-08-30 21:17:03 +0200

    test: Add Clean Architecture tests, EF core and Flyway migration sync tests

---
+++ test/DotNetAtlas.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using DotNetAtlas.Api.Common;
+using DotNetAtlas.Application.Common;
+using DotNetAtlas.Domain.Entities.Base;
+using DotNetAtlas.Infrastructure.Common;
+
+namespace DotNetAtlas.ArchitectureTests
+{
+    public abstract class BaseTest
+    {
+        protected static readonly Assembly DomainAssembly = typeof(ValueObject).Assembly;
+        protected static readonly Assembly ApplicationAssembly = typeof(ApplicationDependencyInjection).Assembly;
+        protected static readonly Assembly InfrastructureAssembly = typeof(InfrastructureDependencyInjection).Assembly;
+        protected static readonly Assembly PresentationAssembly = typeof(ApiDependencyInjection).Assembly;
+    }
+}
