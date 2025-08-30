73abe8963ceb6cafe3584afffb872914ce4fa6f3
DavidCapcuch <d.capcuch@gmail.com>
2025-08-30 21:02:19 +0200

    feat: Refactor GetForecasts, add Versioning, remove Contracts project -> move request/response to Application

---
+++ src/DotNetAtlas.Application/Forecast/GetForecasts/ForecastResponse.cs
@@ -0,0 +1,13 @@
+namespace DotNetAtlas.Application.Forecast.GetForecasts
+{
+    public class ForecastResponse
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ src/DotNetAtlas.Application/Forecast/GetForecasts/GetForecastsResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNetAtlas.Application.Forecast.GetForecasts
+{
+    public class GetForecastsResponse
+    {
+        public required IAsyncEnumerable<ForecastResponse> Forecasts { get; set; }
+    }
+}
