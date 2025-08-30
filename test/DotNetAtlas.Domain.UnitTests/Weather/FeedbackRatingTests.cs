6da07db747e1711ecbae58ea7f306b21cfc8f51a
David Čapčuch <d.capcuch@gmail.com>
2025-08-30 21:14:52 +0200

    test: Add Domain Unit tests

---
+++ test/DotNetAtlas.Domain.UnitTests/Weather/FeedbackRatingTests.cs
@@ -0,0 +1,47 @@
+using AwesomeAssertions;
+using AwesomeAssertions.Execution;
+using DotNetAtlas.Domain.Entities.Weather;
+using DotNetAtlas.Domain.Errors.Base;
+using FluentResults.Extensions.FluentAssertions;
+
+namespace DotNetAtlas.Domain.UnitTests.Weather
+{
+    public class FeedbackRatingTests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void WhenValueWithinRange_ReturnsSuccessWithValue(int value)
+        {
+            // Arrange & Act
+            var result = FeedbackRating.Create(value);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                result.Should().BeSuccess();
+                result.Value.Value.Should().Be(value);
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(6)]
+        [InlineData(-10)]
+        public void WhenValueOutOfRange_ReturnsValidationError(int value)
+        {
+            // Arrange & Act
+            var result = FeedbackRating.Create(value);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                result.Should().BeFailure();
+                var validationError = result.Errors.First() as ValidationError;
+                validationError.Should().NotBeNull();
+                validationError!.ErrorCode.Should().Be("FeedbackRating.OutOfRange");
+            }
+        }
+    }
+}
