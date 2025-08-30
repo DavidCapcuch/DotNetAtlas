887ff013c2a7eab0bb33f44342fd430d07b3f1b0
David Čapčuch <d.capcuch@gmail.com>
2025-08-30 20:57:52 +0200

    feat: Add DEV group, DEV endpoint for seeding database + filter to not show it in production

---
+++ src/DotNetAtlas.Api/Endpoints/Dev/DevGroup.cs
@@ -0,0 +1,22 @@
+﻿using DotNetAtlas.Api.Common.Authentication;
+using FastEndpoints;
+
+namespace DotNetAtlas.Api.Endpoints.Dev
+{
+    public sealed class DevGroup : Group
+    {
+        /// <summary>
+        /// Hidden in production, only accessible in local/development environments.
+        /// </summary>
+        public DevGroup()
+        {
+            Configure(EndpointGroupConstants.DEV, ep =>
+            {
+                ep.Description(builder => builder
+                    .WithGroupName(EndpointGroupConstants.DEV));
+                ep.Tags(EndpointGroupConstants.DEV);
+                ep.Policies(AuthPolicies.DEV_ONLY);
+            });
+        }
+    }
+}
---
+++ src/DotNetAtlas.Api/Endpoints/Dev/SeedDatabaseCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using FastEndpoints;
+using CQS_ICommand = DotNetAtlas.Application.Common.CQS.ICommand;
+using ICommand = DotNetAtlas.Application.Common.CQS.ICommand;
+
+namespace DotNetAtlas.Api.Endpoints.Dev
+{
+    public class SeedDatabaseCommand : CQS_ICommand
+    {
+        /// <summary>
+        /// Number of records to generate.
+        /// </summary>
+        [QueryParam]
+        [DefaultValue(100)]
+        public required int NumberOfRecords { get; set; } = 100;
+    }
+}
